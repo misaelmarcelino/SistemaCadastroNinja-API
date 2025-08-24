769e89f59bd6431a98ec21bffd6821ba13961972
Misael Marcelino <misael.marcelino@outlook.com>
2025-08-23 22:10:15 -0300

    Criada primeira rota da aplicaćao pelo controller de usuario

---
+++ src/main/java/dev/java10x/SistemaDeCadastroNinja/controller/UserController.java
@@ -0,0 +1,16 @@
+package dev.java10x.SistemaDeCadastroNinja.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class UserController {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa é a minha rota inicial";
+    }
+
+}
