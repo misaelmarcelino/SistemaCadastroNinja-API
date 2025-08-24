9da8bbd61face94dc3e0c120e54a51967b1af03d
Misael Marcelino <misael.marcelino@outlook.com>
2025-08-23 21:09:43 -0300

    Iniciando projeto de API de Cadastro Ninja

---
+++ src/main/java/dev/java10x/SistemaDeCadastroNinja/SistemaDeCadastroNinjaApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.SistemaDeCadastroNinja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeCadastroNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeCadastroNinjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/SistemaDeCadastroNinja/SistemaDeCadastroNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.SistemaDeCadastroNinja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeCadastroNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
