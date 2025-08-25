8f95b23d0830dab80e3c3367fe14df2bd314770a
Misael Marcelino <misael.marcelino@outlook.com>
2025-08-24 23:22:35 -0300

    feat: Cria rotas do controller de missoes com Mock

--- src/main/java/dev/java10x/SistemaDeCadastroNinja/MIssoes/controller/MissoesController.java
+++ src/main/java/dev/java10x/SistemaDeCadastroNinja/MIssoes/controller/MissoesController.java
@@ -1,4 +1,29 @@
 package dev.java10x.SistemaDeCadastroNinja.MIssoes.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missoes")
 public class MissoesController {
-}
+
+    // Adicionar novas missoes
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return  "Missao Criada com sucesso";
+    }
+
+    // Listar as missoes
+    @GetMapping
+    public String listarMissao(){
+        return  "Missao Listadas com sucesso";
+    }
+    // Alterar Missao por ID
+    @PutMapping("/alterar/{id}")
+    public String alterarMissao(){
+        return  "Missao Alterada com sucesso";
+    }
+    // Deletar missoes
+    @DeleteMapping("/deletar/{id}")
+    public String deletarMissao(){
+        return  "Missao deletada com sucesso";
+    }
